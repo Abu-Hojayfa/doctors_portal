62d7bac024267ce48c5b6c2de532277bcebed119
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-06 19:23:36 +0600

    main dash top finish

--- src/Components/Dashboard/MainDashboard/MainDashTop/MainDashTop.js
+++ src/Components/Dashboard/MainDashboard/MainDashTop/MainDashTop.js
@@ -1,14 +1,40 @@
 import React from "react";
 import "./MainDashStyle.css";
 
-const MainDashTop = () => {
+const MainDashTop = ({ allAppoints }) => {
+  const todays = new Date().toString();
+  const newDate =
+    todays.split(" ")[0] +
+    " " +
+    todays.split(" ")[1] +
+    " " +
+    todays.split(" ")[2] +
+    " " +
+    todays.split(" ")[3];
+
+  let pendingAppoints = [];
+  let todaysPending = [];
+  for (let i = 0; i < allAppoints.length; i++) {
+    if (allAppoints[i].action === "pending") {
+      pendingAppoints = [...pendingAppoints, allAppoints[i]];
+    }
+    if (allAppoints[i].time === newDate) {
+      todaysPending = [...todaysPending, allAppoints[i]];
+    }
+  }
+
   return (
-    <div className="mainDashTopContainer">
-      <h3 className="pt-4 pb-2">Dashboard</h3>
+    <div className="mainDashTopContainer mb-5">
+      <h3 className="pt-4 pb-3">Dashboard</h3>
       <div className="d-flex justify-content-between">
         <div className="maindashNumTop">
           <div className="d-flex align-items-center">
-            <h5>09</h5>
+            <h5>
+              {pendingAppoints.length >= 0 && pendingAppoints.length < 10
+                ? 0
+                : ""}
+              {pendingAppoints.length}
+            </h5>
             <div>
               <p className="pb-1">Pending</p>
               <p>Appointments</p>
@@ -17,7 +43,10 @@ const MainDashTop = () => {
         </div>
         <div className="maindashNumTop">
           <div className="d-flex align-items-center">
-            <h5>09</h5>
+            <h5>
+              {todaysPending.length >= 0 && todaysPending.length < 10 ? 0 : ""}
+              {todaysPending.length}
+            </h5>
             <div>
               <p className="pb-1">Today's</p>
               <p>Appointments</p>
@@ -26,7 +55,10 @@ const MainDashTop = () => {
         </div>
         <div className="maindashNumTop">
           <div className="d-flex align-items-center">
-            <h5>09</h5>
+            <h5>
+              {allAppoints.length >= 0 && allAppoints.length < 10 ? 0 : ""}
+              {allAppoints.length}
+            </h5>
             <div>
               <p className="pb-1">Total</p>
               <p>Appointments</p>
@@ -35,7 +67,12 @@ const MainDashTop = () => {
         </div>
         <div className="maindashNumTop">
           <div className="d-flex align-items-center">
-            <h5>09</h5>
+            <h5>
+              {(allAppoints.length + 2) >= 0 && (allAppoints.length + 2) < 10
+                ? 0
+                : ""}
+              {allAppoints.length + 2}
+            </h5>
             <div>
               <p className="pb-1">Total</p>
               <p>Patients</p>
--- src/Components/Dashboard/MainDashboard/MainDashboard.js
+++ src/Components/Dashboard/MainDashboard/MainDashboard.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import Sidebar from '../Sidebar/Sidebar';
-import MainDashTop from './MainDashTop/MainDashTop';
+import React, { useEffect, useState } from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import MainDashTop from "./MainDashTop/MainDashTop";
 
 const MainDashboard = () => {
+  const [allAppoints, setAllAppoints] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/allappoints")
+      .then((res) => res.json())
+      .then((data) => setAllAppoints(data));
+  }, []);
   return (
-    <div style={{backgroundColor:'#e5fcff'}}>
+    <div style={{ backgroundColor: "#e5fcff" }}>
       <div className="container-fluid row m-0 ps-0">
         <div className="col-md-2 ps-0">
           <Sidebar />
         </div>
         <div className="col-md-9 ps-4">
-          <MainDashTop />
+          <MainDashTop allAppoints={allAppoints} />
         </div>
       </div>
     </div>
